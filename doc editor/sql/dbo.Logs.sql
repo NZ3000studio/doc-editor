b142a85c923e0a66f9674a91b6ed88309fe49bdf
natan zeitlin <natanzeitlin@gmail.com>
2025-01-27 13:37:18 +0200

    קובץ sql

---
+++ doc editor/sql/dbo.Logs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Logs] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Log]     NVARCHAR (200) NOT NULL,
+    [LogData] NVARCHAR (200) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
